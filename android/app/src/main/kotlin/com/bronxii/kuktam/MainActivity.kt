2f1b1443bf690963b3d5f009611068e32c759c58
Bronxii <gabor.medve.kiraly@gmail.com>
2026-07-17 19:11:26 +0200

    Fix Android package and run app

---
+++ android/app/src/main/kotlin/com/bronxii/kuktam/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bronxii.kuktam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/kuktam/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kuktam
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
